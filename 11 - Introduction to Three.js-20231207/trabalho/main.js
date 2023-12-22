d79bd5e07ace5df97a3dd00592ef91f613a87575
BensonTerra <benson.terra@hotmail.com>
2023-12-22 13:15:21 +0000

    save

--- 11 - Introduction to Three.js-20231207/trabalho/main.js
+++ 11 - Introduction to Three.js-20231207/trabalho/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
 /*********************
 * SCENE 
@@ -12,8 +13,8 @@ const scene = new THREE.Scene();
  * *******************/
 // create a camera, which defines where we're looking at
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5
-camera.position.y = 0
+camera.position.z = 0
+camera.position.y = 7
 camera.position.x = 0;
 
 /*********************
@@ -40,7 +41,11 @@ const base = new THREE.Mesh(geometry, material);
 base.position.y = 0.05/2
 scene.add(base);
 
-let a = 0.6
+//braço
+let a = 2
+let b = 2
+let c = 1
+let r2 = 0.2
 
 geometry = new THREE.CylinderGeometry( 0.4, 0.4, a, 32 ); 
 // let material = new THREE.MeshNormalMaterial({ wireframe: false });
@@ -49,119 +54,71 @@ const eixo1 = new THREE.Mesh(geometry, material);
 eixo1.position.y = 0.03 + (a/2)
 base.add(eixo1);
 
-let b = 1
-let r2 = 0.2
-
 geometry = new THREE.CylinderGeometry( r2, r2, b, 32 ); 
 material = new THREE.MeshBasicMaterial({ color: "green", wireframe: false });
 const eixo2 = new THREE.Mesh(geometry, material);
-eixo2.position.x = 0.75
-eixo2.position.y = 0
+eixo2.position.x = b/2
+eixo2.position.y = 0.75
 eixo2.rotation.z = Math.PI/2
+eixo2.rotation.x = 0
 eixo1.add(eixo2);
 
-geometry = new THREE.SphereGeometry( 0.32, 32, 16 ); 
-material = new THREE.MeshBasicMaterial({ color: "blue", wireframe: false });
+geometry = new THREE.CylinderGeometry( r2, r2, c, 32 ); 
+material = new THREE.MeshBasicMaterial({ color: "red", wireframe: false });
 const eixo3 = new THREE.Mesh(geometry, material);
-eixo3.position.x = 0
+eixo3.position.x = -c/2
 eixo3.position.y = -0.75
+eixo3.rotation.z = Math.PI/2
 eixo2.add(eixo3);
 
-geometry = new THREE.CylinderGeometry( r2, r2, b, 32 ); 
+geometry = new THREE.CylinderGeometry( r2, r2, c, 32 ); 
 material = new THREE.MeshBasicMaterial({ color: "green", wireframe: false });
 const eixo4 = new THREE.Mesh(geometry, material);
-eixo4.position.x = 0.75
+eixo4.position.x = -c/2
+eixo4.position.y = 0.25
 eixo4.rotation.z = Math.PI/2
 eixo3.add(eixo4);
-
-geometry = new THREE.SphereGeometry( 0.32, 32, 16 ); 
-material = new THREE.MeshBasicMaterial({ color: "blue", wireframe: false });
-const eixo5 = new THREE.Mesh(geometry, material);
-eixo5.position.y = -0.75
-eixo4.add(eixo5);
-
-geometry = new THREE.CylinderGeometry( r2, r2, b, 32 ); 
-material = new THREE.MeshBasicMaterial({ color: "green", wireframe: false });
-const eixo6 = new THREE.Mesh(geometry, material);
-eixo6.position.x = 0.75
-eixo6.rotation.z = Math.PI/2
-eixo5.add(eixo6);
-
-geometry = new THREE.SphereGeometry( 0.32, 32, 16 ); 
-material = new THREE.MeshBasicMaterial({ color: "blue", wireframe: false });
-const eixo7 = new THREE.Mesh(geometry, material);
-eixo7.position.y = -0.75
-eixo6.add(eixo7);
-
-geometry = new THREE.CylinderGeometry( r2, r2, b, 32 ); 
-material = new THREE.MeshBasicMaterial({ color: "green", wireframe: false });
-const eixo8 = new THREE.Mesh(geometry, material);
-eixo8.position.x = -0.75
-eixo8.rotation.z = Math.PI/2
-eixo7.add(eixo8);
-
-geometry = new THREE.SphereGeometry( 0.32, 32, 16 ); 
-material = new THREE.MeshBasicMaterial({ color: "blue", wireframe: false });
-const eixo9 = new THREE.Mesh(geometry, material);
-eixo9.position.y = 0.75
-eixo8.add(eixo9);
-
-geometry = new THREE.CylinderGeometry( r2, r2, b, 32 ); 
-material = new THREE.MeshBasicMaterial({ color: "green", wireframe: false });
-const eixo10 = new THREE.Mesh(geometry, material);
-eixo10.position.x = 0.75
-eixo10.rotation.z = Math.PI/2
-eixo9.add(eixo10);
-
-geometry = new THREE.SphereGeometry( 0.32, 32, 16 ); 
-material = new THREE.MeshBasicMaterial({ color: "blue", wireframe: false });
-const eixo11 = new THREE.Mesh(geometry, material);
-eixo11.position.y = -0.75
-eixo10.add(eixo11);
-
-geometry = new THREE.CylinderGeometry( r2, r2, b, 32 ); 
-material = new THREE.MeshBasicMaterial({ color: "green", wireframe: false });
-const eixo12 = new THREE.Mesh(geometry, material);
-eixo12.position.x = 0.75
-eixo12.rotation.z = Math.PI/2
-eixo11.add(eixo12);
-
-geometry = new THREE.SphereGeometry( 0.32, 32, 16 ); 
-material = new THREE.MeshBasicMaterial({ color: "blue", wireframe: false });
-const eixo13 = new THREE.Mesh(geometry, material);
-eixo13.position.y = -0.75
-eixo12.add(eixo13);
-
-geometry = new THREE.CylinderGeometry( r2, r2, b, 32 ); 
-material = new THREE.MeshBasicMaterial({ color: "green", wireframe: false });
-const eixo14 = new THREE.Mesh(geometry, material);
-eixo14.position.x = 0.75
-eixo14.rotation.z = Math.PI/2
-eixo13.add(eixo14);
-
-/** fim */
-geometry = new THREE.SphereGeometry( 0.32, 32, 16 ); 
-material = new THREE.MeshBasicMaterial({ color: "blue", wireframe: false });
-const eixo15 = new THREE.Mesh(geometry, material);
-eixo15.position.y = -0.75
-eixo15.rotation.z = Math.PI/2
-eixo14.add(eixo15);
 
 const axesHelper = new THREE.AxesHelper(5);
 eixo1.add(axesHelper);
 
-const axesHelper2 = new THREE.AxesHelper(5);
-eixo15.add(axesHelper2);
-
 // start the animation
 renderer.setAnimationLoop(render);
 
+//GUI
+/*
+const gui = new GUI()
+
+const eixo1Folder = gui.addFolder('Eixo1')
+eixo1Folder.add(eixo1.rotation, 'y', 0, Math.PI * 2)
+eixo1Folder.open()
+
+gui.close()
+*/
+
+/*
+const eixoTeste = eixo4
+
+const axesHelper2 = new THREE.AxesHelper(5);
+eixoTeste.add(axesHelper2);
 
 
+const eixoFolder = gui.addFolder('EixoTeste')
+eixoFolder.add(eixoTeste.position, 'x', -5,5)
+eixoFolder.add(eixoTeste.position, 'y', -5,5)
+eixoFolder.add(eixoTeste.position, 'z', -5,5)
+eixoFolder.add(eixoTeste.rotation, 'x', 0,Math.PI)
+eixoFolder.add(eixoTeste.rotation, 'y', 0,Math.PI)
+eixoFolder.add(eixoTeste.rotation, 'z', 0,Math.PI)
+eixoFolder.open()
 
+setInterval(() => {
+    // teste()
+    console.log(eixo2.position)
+}, 1000);
+*/
+function render() {  
 
-
-function render() {    
     controls.update();
     renderer.render(scene, camera);
 };
